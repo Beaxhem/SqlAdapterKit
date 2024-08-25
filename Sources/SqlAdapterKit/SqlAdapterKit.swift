ee2dd3925a8d134bf72c3c3beac82c6640d334f0
Illia Senchukov <senchukov02@gmail.com>
2024-08-25 12:27:06 +0300

    feat: QueryError

--- Sources/SqlAdapterKit/SqlAdapterKit.swift
+++ Sources/SqlAdapterKit/SqlAdapterKit.swift
@@ -1,13 +1,15 @@
 public protocol SqlAdapter {
-    static func connect(configuration: Configuration) -> Result<Self, Error>
+    static func connect(configuration: Configuration) -> Result<Self, QueryError>
 
-    func query(_ query: String) -> Result<[String], Error>
+    func query(_ query: String) -> Result<[String], QueryError>
     func execute(_ query: String)
 }
 
 public struct DBService<Adapter: SqlAdapter> {
 
-    let adapter: Adapter
+    public var isConnected: Bool { adapter != nil }
+
+    var adapter: Adapter?
 
     public init(adapter: Adapter) {
         self.adapter = adapter
@@ -17,10 +19,29 @@ public struct DBService<Adapter: SqlAdapter> {
 
 public extension DBService {
 
-    func query(_ query: String) {
-        let _ = adapter.query(query)
+    func query(_ query: String) throws (QueryError) {
+        guard let adapter else { return }
+
+        switch adapter.query(query) {
+        case .success:
+            return
+        case .failure(let error):
+            throw error
+        }
+    }
+
+    mutating func disconnect() {
+        adapter = nil
     }
 
 }
 
+public struct QueryError: Error {
 
+    public var message: String
+
+    public init(message: String) {
+        self.message = message
+    }
+
+}
